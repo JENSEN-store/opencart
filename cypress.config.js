15d88e5d2da1a73788b1ccf0ee03c1667ea5d2fe
Swakshar Dutta <tarzan0607@gmail.com>
2024-01-26 14:42:28 +0100

    disable image loading on chronium browers

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          launchOptions.args.push('--blink-settings=imagesEnabled=false')
+          return launchOptions
+        }
+      })
     },
   },
 });
