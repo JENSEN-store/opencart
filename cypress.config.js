41b2a9b0b916b57f4b437dbbb3baa367eeb8febe
Swakshar Dutta <tarzan0607@gmail.com>
2024-01-31 11:16:43 +0100

    fix tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
-    setupNodeEvents(on, config) {
-      
-    },
+    baseUrl: "http://localhost",
   },
 });
