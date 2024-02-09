cfd6d64188572d0d777b8d26f71ff12c77523ff1
JamJarLid <espen.lever@gmail.com>
2024-02-09 11:04:39 +0100

    changed path type

--- Terraform/ingress.tf
+++ Terraform/ingress.tf
@@ -22,7 +22,8 @@ resource "kubernetes_ingress_v1" "opencart" {
       host = "opencart.jamjarlid.com"
       http {
         path {
-          path = "/*"
+          path = "/"
+          path_type = "Prefix"
           backend {
             service {
               name = "opencart-service"
